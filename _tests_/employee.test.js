7e5946c4217749b0bba02530a3b988845de8588d
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-19 16:22:51 -0700

    Added new tests to check that new employee is taking arguments correctly and that it will throw an Error if User does not input correctly

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -14,5 +14,35 @@ describe("Employee", () => {
             const obj = new Employee();
             expect("email" in obj).toEqual(true)
         })
+        it('Should have a properties that are equal to the arguments that were passed in', () => {
+            const name = "James"
+            const result = new Employee(name, 1, "anon@gmail.com")
+            expect(result.name).toEqual(name)
+        })
+        it('Should have a properties that are equal to the arguments that were passed in', () => {
+            const id = 1
+            const result = new Employee("James", id, "anon@gmail.com")
+            expect(result.id).toEqual(id)
+        })
+        it('Should have a properties that are equal to the arguments that were passed in', () => {
+            const email = "anon@gmail.com"
+            const result = new Employee("James", 4, email)
+            expect(result.email).toEqual(email)
+        })
+        it("Should throw an error if a valid name was not provided", () => {
+            const callbackFn = () => new Employee();
+            const expectedError = new Error("Name must be a string and not empty")
+            expect(callbackFn).toThrowError(expectedError)
+        })
+        it("Should throw an error if a valid ID was not provided", () => {
+            const callbackFn = () => new Employee();
+            const expectedError = new Error("ID must be a number and not empty")
+            expect(callbackFn).toThrowError(expectedError)
+        })
+        it("Should throw an error if a valid email was not provided", () => {
+            const callbackFn = () => new Employee();
+            const expectedError = new Error("Email must be a string and not empty")
+            expect(callbackFn).toThrowError(expectedError)
+        })
     })
 })
