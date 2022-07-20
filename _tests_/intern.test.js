08bb2dc84b8a102e9a18239293b45266423939d6
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-20 13:34:26 -0700

    if implemented this commit will: 1. Refactor all files to work with export and imports of ES6

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -1,5 +1,5 @@
-const Employee = require('../lib/Employee');
-const Intern = require('../lib/Intern');
+import {Employee} from "../lib/Employee.js";
+import {Intern} from '../lib/Intern.js';
 
 describe(`Intern`, () => {
     describe(`Init`, () => {
--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const fs = require("node:fs");
+import fs from "node:fs";
 import inquirer from "inquirer";
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+import {Employee} from './lib/Employee.js';
+import {Manager} from './lib/Manager.js';
+import {Engineer} from './lib/Engineer.js';
+import {Intern} from './lib/Intern.js';
+
+const managerQuestions = [
+    {
+        message: "What is the Manager of your team's name?",
+        name: "mangerName",
+        type: "input",
+    },
+    {
+        message: "What is their Employee ID number?",
+        name: "managerID",
+        type: "number",
+    },
+    {
+        message: "What is their email address?",
+        name: "managerEmail",
+        type: "input",
+    },
+    {
+        message: "What is their Office Number?",
+        name: "officeNumber",
+        type: "number",
+    },
+]
 
 
+function init() {
+    inquirer.prompt(managerQuestions).then((inquirerResponses) => {
+        console.log(inquirerResponses);
+    })
+}
+init()
