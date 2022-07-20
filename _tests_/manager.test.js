cece4989a762c8a5df306b3db470dc8c894f4987
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-19 23:43:58 -0700

    If implemented this commit will: 1. Refactor Employee role property and getRole method. 2. Add new methods for getOfficeNumber and getRole 3. Add an error catch. 4. Add tests for the Manager Class to ensure all properties and methods work

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -4,12 +4,33 @@ const Manager  = require('../lib/Manager');
 describe(`Manager`, () => {
     describe(`Init`, () => {
         it(`Should create an instance of "Manager" that is an extension of "Employee"`, () => {
-            const newManager = new Manager("James", 1, "anon@anon.com", 48)
-            expect(newManager instanceof Employee).toEqual(true)
-        })
+            const newManager = new Manager("James", 1, "anon@anon.com", 48);
+            expect(newManager instanceof Employee).toEqual(true);
+        });
         it(`Should have an "officeNumber" property equal to the argument passed in`, () => {
-            const newManager = new Manager("James", 1, "anon@anon.com", 48)
-            expect(newManager.officeNumber).toEqual(48)
+            const newManager = new Manager("James", 1, "anon@anon.com", 48);
+            expect(newManager.officeNumber).toEqual(48);
+        });
+        it(`Should throw an Error if a valid input for officeNumber was not provided`, () => {
+            const officeNumber = "";
+            const callbackFn = () => new Manager("James", 1, "anon@anon.com", officeNumber);
+            const expectedError = 'officeNumber must be a number and not empty';
+            expect(callbackFn).toThrow(expectedError);
+        });
+    });
+    describe(`getOfficeNumber method`, () => {
+        it(`Should return the "officeNumber" properties value`, () => {
+            const newManager = new Manager("James", 1, "anon@anon.com", 48);
+            const result = newManager.getOfficeNumber();
+            expect(result).toEqual(newManager.officeNumber);
+        });
+    });
+    describe(`getRole method`, () => {
+        it(`Should return the "role" property with a value of "Manager"`, () => {
+            const newManager = new Manager("James", 1, "anon@anon.com", 48);
+            const result = newManager.getRole()
+            expect(result).toEqual("Manager")
+
         })
     })
 })
