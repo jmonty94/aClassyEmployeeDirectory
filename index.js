31f307a26c843d9398ba92ba79ee7b1b3b2802cd
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-20 12:39:28 -0700

    Added imports for all needed modules

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require("node:fs");
+import inquirer from "inquirer";
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+
