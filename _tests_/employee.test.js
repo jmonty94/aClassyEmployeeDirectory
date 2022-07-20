b42a94b2457cb4eb036d250a4e542b9bb3a22397
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-19 17:55:56 -0700

    if implemented this commit will: 1. Refactor tests to be less WET 2. Allow all 5 in "Init" suite to pass

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -2,47 +2,43 @@ const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
     describe("Init", () => {
-        it("Should return an object containing a 'name' property when called with the 'new' keyword", () => {
-            const obj = new Employee();
-            expect("name" in obj).toEqual(true)
-        })
-        it("Should return an object containing a 'id' property when called with the 'new' keyword", () => {
-            const obj = new Employee();
-            expect("id" in obj).toEqual(true)
-        })
-        it("Should return an object containing a 'email' property when called with the 'new' keyword", () => {
-            const obj = new Employee();
-            expect("email" in obj).toEqual(true)
-        })
+        it("Should return an object containing a 'name', 'id', and 'email' property when called with the 'new' keyword", () => {
+            const name = "James";
+            const id = 1;
+            const email = "anon@gmail.com";
+            const newEmployee = new Employee(name, id, email);
+            expect("name" in newEmployee && "id" in newEmployee && "email" in newEmployee).toEqual(true);
+        });
         it('Should have a properties that are equal to the arguments that were passed in', () => {
-            const name = "James"
-            const result = new Employee(name, 1, "anon@gmail.com")
-            expect(result.name).toEqual(name)
-        })
-        it('Should have a properties that are equal to the arguments that were passed in', () => {
-            const id = 1
-            const result = new Employee("James", id, "anon@gmail.com")
-            expect(result.id).toEqual(id)
-        })
-        it('Should have a properties that are equal to the arguments that were passed in', () => {
-            const email = "anon@gmail.com"
-            const result = new Employee("James", 4, email)
-            expect(result.email).toEqual(email)
-        })
+            const name = "James";
+            const id = 1;
+            const email = "anon@gmail.com";
+            const result = new Employee(name, id, email);
+            expect(result.name && result.id && result.email).toEqual(name && id && email);
+        });
         it("Should throw an error if a valid name was not provided", () => {
-            const callbackFn = () => new Employee();
-            const expectedError = new Error("Name must be a string and not empty")
-            expect(callbackFn).toThrowError(expectedError)
-        })
+            const name = null || " " || 4;
+            const id = 1;
+            const email = "anon@gmail.com";
+            const callbackFn = () => new Employee(name, id, email);
+            const expectedError = new Error("Name must be a string and not empty");
+            expect(callbackFn).toThrowError(expectedError);
+        });
         it("Should throw an error if a valid ID was not provided", () => {
-            const callbackFn = () => new Employee();
-            const expectedError = new Error("ID must be a number and not empty")
-            expect(callbackFn).toThrowError(expectedError)
-        })
+            const name = "James";
+            const id = "1" || null;
+            const email = "anon@gmail.com";
+            const callbackFn = () => new Employee(name, id, email);
+            const expectedError = new Error("ID must be a number and not empty");
+            expect(callbackFn).toThrowError(expectedError);
+        });
         it("Should throw an error if a valid email was not provided", () => {
-            const callbackFn = () => new Employee();
-            const expectedError = new Error("Email must be a string and not empty")
-            expect(callbackFn).toThrowError(expectedError)
-        })
-    })
-})
+            const name = "James";
+            const id = 1;
+            const email = " " || 4;
+            const callbackFn = () => new Employee(name, id, email);
+            const expectedError = new Error("Email must be a string and not empty");
+            expect(callbackFn).toThrowError(expectedError);
+        });
+    });
+});
