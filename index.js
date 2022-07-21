d3b04dad1776e8a90478598381cd08cad269dafb
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-20 22:09:48 -0700

    If Implemented this commit will: 1. put the questions into their own file in the src folder 2. create generateEmployeeDirectory.js file in the src folder 3. create the beginning of the inquirer functions needed for employees

--- index.js
+++ index.js
@@ -4,34 +4,44 @@ import {Employee} from './lib/Employee.js';
 import {Manager} from './lib/Manager.js';
 import {Engineer} from './lib/Engineer.js';
 import {Intern} from './lib/Intern.js';
+import {managerQuestions, addEmployee, engineerQuestions, internQuestions} from './src/questions.js'
 
-const managerQuestions = [
-    {
-        message: "What is the Manager of your team's name?",
-        name: "mangerName",
-        type: "input",
-    },
-    {
-        message: "What is their Employee ID number?",
-        name: "managerID",
-        type: "number",
-    },
-    {
-        message: "What is their email address?",
-        name: "managerEmail",
-        type: "input",
-    },
-    {
-        message: "What is their Office Number?",
-        name: "officeNumber",
-        type: "number",
-    },
-]
+async function init() {
+    await inquirer.prompt(managerQuestions).then((answers) => {
+        // console.log(answers);
+        const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
 
+        addNewEmployee();
+    });
+};
 
-function init() {
-    inquirer.prompt(managerQuestions).then((inquirerResponses) => {
-        console.log(inquirerResponses);
+const addNewEmployee = function() {
+    inquirer.prompt(addEmployee).then((answers) => {
+        let answer = answers.employee;
+        if (answer === `Engineer`) {
+            addEngineer();
+        } else if (answer === `Intern`) {
+            addIntern();
+        } else if (answer === `No thanks`){
+            console.log(`Creating HTML`);
+        }
     })
-}
+};
+const addEngineer = function() {
+    inquirer.prompt(engineerQuestions).then((answers) => {
+        const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        addNewEmployee();
+    });
+};
+const addIntern = function() {
+    inquirer.prompt(internQuestions).then((answers) => {
+        const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        addNewEmployee();
+    });
+};
+
+
+
 init()
+
+
---
+++ src/generateEmployeeDirectory.js
@@ -0,0 +1,4 @@
+import {Employee} from './lib/Employee.js';
+import {Manager} from './lib/Manager.js';
+import {Engineer} from './lib/Engineer.js';
+import {Intern} from './lib/Intern.js';
