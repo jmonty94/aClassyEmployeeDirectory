fd062e257df1ed72d62249baeaa3dba23bf6f5e6
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-07-21 16:42:18 -0700

    This commit will: 1. finish the HTML generator functions and add said fucntion to the index.js 2. create a new file for example purposes in the dist folder

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import {Manager} from './lib/Manager.js';
 import {Engineer} from './lib/Engineer.js';
 import {Intern} from './lib/Intern.js';
 import {managerQuestions, addEmployee, engineerQuestions, internQuestions} from './src/questions.js'
+import { addTeamMember, finishedHTML } from './src/generateEmployeeDirectory.js'
 
 async function init() {
     await inquirer.prompt(managerQuestions).then((answers) => {
         // console.log(answers);
         const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
-
+        addTeamMember(manager)
         addNewEmployee();
     });
 };
@@ -22,23 +23,24 @@ const addNewEmployee = function() {
             addIntern();
         } else if (answer === `No thanks`){
             console.log(`Creating HTML`);
+            finishedHTML()
         }
     })
 };
 const addEngineer = function() {
     inquirer.prompt(engineerQuestions).then((answers) => {
         const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        addTeamMember(engineer)
         addNewEmployee();
     });
 };
 const addIntern = function() {
     inquirer.prompt(internQuestions).then((answers) => {
         const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        addTeamMember(intern)
         addNewEmployee();
     });
 };
-
-
 
 init()
 
--- src/generateEmployeeDirectory.js
+++ src/generateEmployeeDirectory.js
@@ -3,7 +3,6 @@ import fs from "node:fs";
 let employeeDirectoryHtml = `
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,7 +15,6 @@ let employeeDirectoryHtml = `
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.css" rel="stylesheet" />
     <title>Our Team</title>
 </head>
-
 <body>
     <header>
         <div class="p-5 text-center bg-image" style="
@@ -32,26 +30,57 @@ let employeeDirectoryHtml = `
             </div>
         </div>
     </header>
-    <main class="container-fuid">
+    <main class="container-fuid d-flex">
 `;
 const addTeamMember = function (data) {
     let roleSpecific = ``;
     let roleIcon = ``
     if (data.getRole() === `Manager`) {
-
+        roleSpecific = `<i class="fas fa-building"></i> Office Number: ${data.officeNumber}`;
+        roleIcon =`<i class="fas fa-chalkboard-teacher"></i>`;
+    } else if (data.getRole() === `Engineer`) {
+        roleSpecific = `<i class="fab fa-github"></i> GitHub profile page : <a href="https://github.com/${data.github}" target="_blank">${data.github}</a>`;
+        roleIcon = `<i class="fas fa-people-carry"></i>`;
+    } else if (data.getRole() === `Intern`){
+        roleSpecific = `<i class="fas fa-graduation-cap"></i> School attending: ${data.school}`;
+        roleIcon = `<i class="fas fa-mug-hot"></i>`
     }
     let cardContent =
         `
-    <div class="card text-center">
+    <div class="card col-3 m-5 px-auto text-center">
         <div class="card-header">${roleIcon}${data.getRole()}</div>
         <div class="card-body">
-            <h5 class="card-title>${data.name}</h5>
+            <h5 class="card-title">${data.name}</h5>
             <ul class="list-group list-group-light list-group-small">
                 <li class="list-group-item px-4">Employee ID: ${data.id}</li>
-                <li class="list-group-item px-4">Employee Email: ${data.email}</li>
+                <li class="list-group-item px-4">Employee Email: <a href="mailto:${data.email}">${data.email}</a> </li>
                 <li class="list-group-item px-4">${roleSpecific}</li>
             </ul>
         </div>
     </div>
+    `;
+    employeeDirectoryHtml += cardContent
+};
+const htmlEnding = function () {
+    employeeDirectoryHtml += 
     `
+    </main>
+    </body>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.3.0/mdb.min.js"></script>
+    </html>
+    `;
+    return employeeDirectoryHtml
 }
+
+const finishedHTML = function(){
+    const completedFile = htmlEnding();
+    fs.writeFile(`./dist/index.html`, completedFile, error => {
+        if(error) {
+            console.log(error);
+            return;
+        };
+        console.log(`Employee directory completed and published in ./dist under index.html`);
+    });
+};
+
+export { addTeamMember, finishedHTML }
